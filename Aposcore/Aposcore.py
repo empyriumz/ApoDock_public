aa85c60854441ecf7396507958ff4083dcdee0e2
empyriumz <xindai90@gmail.com>
2025-02-24 22:22:36 +0000

    WIP: try to get a minimal version for inference

--- Aposcore/Aposcore.py
+++ Aposcore/Aposcore.py
@@ -1,51 +1,94 @@
-# %%
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.nn import Linear
-from torch_geometric.nn import global_mean_pool
-from HIL import GVP_embedding, InteractionBlock,MPNNL,CrossAttentionBlock
+from HIL import GVP_embedding, InteractionBlock, MPNNL
 from torch_geometric.utils import to_dense_batch
-import numpy as np
+
 
 class Aposcore(nn.Module):
-    def __init__(self, node_dim, hidden_dim, num_heads, dropout, crossAttention, atten_active_fuc, num_layers, interact_type, n_gaussians=10):
+    def __init__(
+        self,
+        node_dim,
+        hidden_dim,
+        num_heads,
+        dropout,
+        crossAttention,
+        atten_active_fuc,
+        num_layers,
+        interact_type,
+        n_gaussians=10,
+    ):
         super().__init__()
         self.interact_type = interact_type
 
-        self.lin_node_lig = nn.Sequential(Linear(node_dim, hidden_dim),nn.LeakyReLU())
+        self.lin_node_lig = nn.Sequential(Linear(node_dim, hidden_dim), nn.LeakyReLU())
 
-        self.conv = nn.ModuleList([MPNNL(hidden_dim, hidden_dim, dropout) for _ in range(num_layers)])
-       
+        self.conv = nn.ModuleList(
+            [MPNNL(hidden_dim, hidden_dim, dropout) for _ in range(num_layers)]
+        )
 
-        self.conv_protein = GVP_embedding((6, 3), (hidden_dim, 16),
-                                            (32, 1), (32, 1), seq_in=True, num_layers=num_layers, drop_rate=dropout, plm=False)
-        
-        # self.CrossAttent = CrossAttentionBlock(hidden_dim, num_heads, dropout)
-        self.interaction = InteractionBlock(hidden_dim, num_heads, dropout, atten_active_fuc, crossAttention, interact_type)
+        self.conv_protein = GVP_embedding(
+            (6, 3),
+            (hidden_dim, 16),
+            (32, 1),
+            (32, 1),
+            seq_in=True,
+            num_layers=num_layers,
+            drop_rate=dropout,
+            plm=False,
+        )
+        self.interaction = InteractionBlock(
+            hidden_dim,
+            num_heads,
+            dropout,
+            atten_active_fuc,
+            crossAttention,
+            interact_type,
+        )
         self.z_pi = nn.Sequential(
-                    nn.Linear(hidden_dim, n_gaussians),
-                    nn.Softmax(dim=-1),
-                    ) # pi:
+            nn.Linear(hidden_dim, n_gaussians),
+            nn.Softmax(dim=-1),
+        )  # pi:
         self.z_mu = nn.Sequential(
-                    nn.Linear(hidden_dim, n_gaussians),
-                    nn.LeakyReLU(),
-                    )
+            nn.Linear(hidden_dim, n_gaussians),
+            nn.LeakyReLU(),
+        )
         self.z_sigma = nn.Sequential(
-                    nn.Linear(hidden_dim, n_gaussians),
-                    nn.LeakyReLU(),
-                    )
-
+            nn.Linear(hidden_dim, n_gaussians),
+            nn.LeakyReLU(),
+        )
 
     def forward(self, data):
-        # lig_scope = data['ligand_scope']
-        # amino_acid_scope = data['protein_scope']
-        data_l = data['ligand_features']
-        data_aa = data['protein_features']
+        data_l = data["ligand_features"]
+        data_aa = data["protein_features"]
 
-        x_l,  edge_index_l,  x_aa, seq, node_s, node_v, edge_index, edge_s, edges_v,edge_feat_l, pos_l, pos_p = \
-        data_l.x, data_l.edge_index, data_aa.x_aa, data_aa.seq, data_aa.node_s, data_aa.node_v, \
-        data_aa.edge_index, data_aa.edge_s, data_aa.edge_v, data_l.edge_attr, data_l.pos, data_aa.pos
+        (
+            x_l,
+            edge_index_l,
+            x_aa,
+            seq,
+            node_s,
+            node_v,
+            edge_index,
+            edge_s,
+            edges_v,
+            edge_feat_l,
+            pos_l,
+            pos_p,
+        ) = (
+            data_l.x,
+            data_l.edge_index,
+            data_aa.x_aa,
+            data_aa.seq,
+            data_aa.node_s,
+            data_aa.node_v,
+            data_aa.edge_index,
+            data_aa.edge_s,
+            data_aa.edge_v,
+            data_l.edge_attr,
+            data_l.pos,
+            data_aa.pos,
+        )
         # print('x_l:', x_l.shape)
         x_l = self.lin_node_lig(x_l)
         for conv in self.conv:
@@ -54,58 +97,56 @@ class Aposcore(nn.Module):
         nodes = (node_s, node_v)
         edges = (edge_s, edges_v)
         protein_out = self.conv_protein(nodes, edge_index, edges, seq)
-        # print('protein_out:', protein_out.shape)
-        # print('pos_l:', pos_l.shape)
-        # print('pos_p:', pos_p.shape)
         x_l, mask_l = to_dense_batch(x_l, data_l.batch, fill_value=0)
         protein_out, mask_p = to_dense_batch(protein_out, data_aa.batch, fill_value=0)
-        pos_l, mask_pos_l = to_dense_batch(pos_l, data_l.batch, fill_value=0)
-        pos_p, mask_pos_p = to_dense_batch(pos_p, data_aa.batch, fill_value=0)
-        # print("x_l",x_l.shape)
-        # print("protein_out",protein_out.shape)
-        # print("pos_l",pos_l.shape)
-        # print("pos_p",pos_p.shape)
-        (B, N_l, C_out), N_p = x_l.size(), protein_out.size(1)
+        pos_l, _ = to_dense_batch(pos_l, data_l.batch, fill_value=0)
+        pos_p, _ = to_dense_batch(pos_p, data_aa.batch, fill_value=0)
+        (B, N_l, _), N_p = x_l.size(), protein_out.size(1)
         Interact, Interact_mask = self.interaction(x_l, protein_out, mask_l, mask_p)
         self.B = B
         self.N_l = N_l
         self.N_p = N_p
 
         ## Get batch indexes for ligand-target combined features
-        C_batch =torch.tensor(range(B)).unsqueeze(-1).unsqueeze(-1).repeat(1, N_l, N_p)
+        device = x_l.device  # Get device from input tensor
+        C_batch = (
+            torch.tensor(range(B), device=device)
+            .unsqueeze(-1)
+            .unsqueeze(-1)
+            .repeat(1, N_l, N_p)
+        )
         C_batch = C_batch[Interact_mask]
-        
-        
-        dist = self.compute_euclidean_distances_matrix(pos_l, pos_p.view(B,-1,3))[Interact_mask]
+
+        dist = self.compute_euclidean_distances_matrix(pos_l, pos_p.view(B, -1, 3))[
+            Interact_mask
+        ]
         # print(self.compute_euclidean_distances_matrix(pos_l, pos_p.view(B,-1,3)).shape)
         pi = self.z_pi(Interact)
-        sigma =self.z_sigma(Interact) +1.1
+        sigma = self.z_sigma(Interact) + 1.1
         sigma = torch.clamp(sigma, min=1e-6)
-        mu = self.z_mu(Interact) +1
-        
-        # print(dist.shape)
-        # print("pi",pi.dtype)
-        # print("sigma",sigma.dtype)
-        # print("mu",mu.dtype)
-        # print("dist",dist.dtype)
+        mu = self.z_mu(Interact) + 1
 
         return pi, sigma, mu, dist.unsqueeze(1).detach(), C_batch
-    
+
     def compute_euclidean_distances_matrix(self, X, Y):
-        # Based on: https://medium.com/@souravdey/l2-distance-matrix-vectorization-trick-26aa3247ac6c
-        # (X-Y)^2 = X^2 + Y^2 -2XY
-        X = X.double()  # (B, N, 3)
-        Y = Y.double()  # (B, M, 3)
-        # print(-2 * torch.bmm(X, Y.permute(0, 2, 1))) # (B, N, M)
-        dists = -2 * torch.bmm(X, Y.permute(0, 2, 1)) + torch.sum(Y**2, axis=-1).unsqueeze(1) + torch.sum(X**2, axis=-1).unsqueeze(-1) #
-        # print(dists.shape)
-        return torch.nan_to_num((dists**0.5).view(self.B, self.N_l,-1, 24),10000).min(axis=-1)[0] 
+        X = X.double()
+        Y = Y.double()
+
+        dists = (
+            -2 * torch.bmm(X, Y.permute(0, 2, 1))
+            + torch.sum(Y**2, axis=-1).unsqueeze(1)
+            + torch.sum(X**2, axis=-1).unsqueeze(-1)
+        )
+
+        return torch.nan_to_num(
+            (dists**0.5).view(self.B, self.N_l, -1, 24), 10000
+        ).min(axis=-1)[0]
 
     def mdn_loss(self, pi, sigma, mu, y, eps1=1e-10, eps2=1e-10):
 
         normal = torch.distributions.Normal(mu, sigma)
-        loglik = normal.log_prob(y.expand_as(normal.loc)) 
-        prob = (torch.log(pi + eps1) + loglik).exp().sum(1) 
+        loglik = normal.log_prob(y.expand_as(normal.loc))
+        prob = (torch.log(pi + eps1) + loglik).exp().sum(1)
         loss = -torch.log(prob + eps2)
 
         return loss, prob
@@ -113,20 +154,20 @@ class Aposcore(nn.Module):
     def calculate_probablity(self, pi, sigma, mu, y):
         normal = torch.distributions.Normal(mu, sigma)
         logprob = normal.log_prob(y.expand_as(normal.loc))
-        logprob += torch.log(pi) 
-        prob = logprob.exp().sum(1)      
+        logprob += torch.log(pi)
+        prob = logprob.exp().sum(1)
         return prob
-        
+
     def sample(self, pi, sigma, mu):
-        k = torch.multinomial(pi, 1).squeeze(-1) # get the index of the gaussian
-        indices = torch.tensor(range(self.B)).unsqueeze(-1).repeat(1, self.N_l).unsqueeze(-1)
+        k = torch.multinomial(pi, 1).squeeze(-1)  # get the index of the gaussian
+        indices = (
+            torch.tensor(range(self.B)).unsqueeze(-1).repeat(1, self.N_l).unsqueeze(-1)
+        )
         indices = indices[k == 1].squeeze(-1)
         mu = mu[indices]
         sigma = sigma[indices]
         normal = torch.distributions.Normal(mu, sigma)
-        return normal.sample()  # 
-
-
+        return normal.sample()  #
 
 
 class FC(nn.Module):
@@ -156,18 +197,3 @@ class FC(nn.Module):
             h = layer(h)
 
         return h
-
-if __name__ == '__main__':
-    import os
-    import pandas as pd
-    from dataset_ConBAP import GraphDataset, PLIDataLoader
-    data_root = './data/pdbbind/'
-    data_df = pd.read_csv(os.path.join(data_root, 'data.csv')).head(10)
-    
-    # # three hours
-    toy_set = GraphDataset(data_root, data_df, graph_type='ConBAP', dis_threshold=8, create=False)
-    toy_loader = PLIDataLoader(toy_set, batch_size=4, shuffle=True)
-    model = bindingsite(35, 256)
-    for data in toy_loader:
-       loss,out = model(data)
-       print( loss, out.shape)
--- Aposcore/HIL.py
+++ Aposcore/HIL.py
@@ -1,18 +1,15 @@
-
 import torch
 from torch import Tensor
 from torch_geometric.nn.conv import MessagePassing
 import torch.nn as nn
 import torch.nn.functional as F
-from gvp import GVP, GVPConvLayer, LayerNorm 
+from gvp import GVP, GVPConvLayer, LayerNorm
+
 
 # heterogeneous interaction layer
 class MPNNL(MessagePassing):
-    def __init__(self, in_channels: int,
-                 out_channels: int, 
-                 dropout,
-                 **kwargs):
-        kwargs.setdefault('aggr', 'add')
+    def __init__(self, in_channels: int, out_channels: int, dropout, **kwargs):
+        kwargs.setdefault("aggr", "add")
         super(MPNNL, self).__init__(**kwargs)
 
         self.in_channels = in_channels
@@ -22,17 +19,20 @@ class MPNNL(MessagePassing):
             nn.Linear(self.in_channels, self.out_channels),
             nn.Dropout(dropout),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(self.out_channels))
+            nn.BatchNorm1d(self.out_channels),
+        )
         self.mlp_node_ncov = nn.Sequential(
             nn.Linear(self.in_channels, self.out_channels),
             nn.Dropout(dropout),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(self.out_channels))
+            nn.BatchNorm1d(self.out_channels),
+        )
         self.edge_mlp = nn.Sequential(
             nn.Linear(6, out_channels),
             nn.Dropout(dropout),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(self.out_channels))
+            nn.BatchNorm1d(self.out_channels),
+        )
 
         self.mlp_coord_cov = nn.Sequential(nn.Linear(9, self.in_channels), nn.SiLU())
         self.mlp_coord_ncov = nn.Sequential(nn.Linear(9, self.in_channels), nn.SiLU())
@@ -40,43 +40,51 @@ class MPNNL(MessagePassing):
             nn.Linear(in_channels + out_channels, out_channels),
             nn.Dropout(dropout),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(self.out_channels))
+            nn.BatchNorm1d(self.out_channels),
+        )
         self.messageNN = nn.Sequential(
-            nn.Linear(in_channels*3, out_channels),
+            nn.Linear(in_channels * 3, out_channels),
             nn.Dropout(dropout),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(self.out_channels))
+            nn.BatchNorm1d(self.out_channels),
+        )
 
-    def forward(self, x, edge_index, edge_features,
-                size=None):
+    def forward(self, x, edge_index, edge_features, size=None):
 
-        out_node_intra = self.propagate(edge_index=edge_index, x=x,edge_features=edge_features,messageNN=self.messageNN, updateNN=self.updateNN , size=size)#radial=radial_cov,
-        out_node = self.mlp_node_cov(x + out_node_intra)  
+        out_node_intra = self.propagate(
+            edge_index=edge_index,
+            x=x,
+            edge_features=edge_features,
+            messageNN=self.messageNN,
+            updateNN=self.updateNN,
+            size=size,
+        )  # radial=radial_cov,
+        out_node = self.mlp_node_cov(x + out_node_intra)
 
         return out_node
 
-    def message(self, x_j: Tensor, x_i: Tensor, messageNN, edge_features,
-                index: Tensor):
+    def message(
+        self, x_j: Tensor, x_i: Tensor, messageNN, edge_features, index: Tensor
+    ):
         edge_feat = self.edge_mlp(edge_features)
-        return messageNN(torch.cat([x_j, x_i,edge_feat], dim=-1)) 
+        return messageNN(torch.cat([x_j, x_i, edge_feat], dim=-1))
 
     def update(self, aggr_out: Tensor, x: Tensor, updateNN):
         return updateNN(torch.cat([aggr_out, x], dim=-1))
 
 
-
 class GVP_embedding(nn.Module):
-    '''
+    """
     Modified based on https://github.com/drorlab/gvp-pytorch/blob/main/gvp/models.py
     GVP-GNN for Model Quality Assessment as described in manuscript.
-    
-    Takes in protein structure graphs of type `torch_geometric.data.Data` 
+
+    Takes in protein structure graphs of type `torch_geometric.data.Data`
     or `torch_geometric.data.Batch` and returns a scalar score for
     each graph in the batch in a `torch.Tensor` of shape [n_nodes]
-    
+
     Should be used with `gvp.data.ProteinGraphDataset`, or with generators
     of `torch_geometric.data.Batch` objects with the same attributes.
-    
+
     :param node_in_dim: node dimensions in input graph, should be
                         (6, 3) if using original features
     :param node_h_dim: node dimensions to use in GVP-GNN layers
@@ -89,13 +97,22 @@ class GVP_embedding(nn.Module):
              is assumed to be part of input node embeddings
     :param num_layers: number of GVP-GNN layers
     :param drop_rate: rate to use in all dropout layers
-    '''
-    def __init__(self, node_in_dim, node_h_dim, 
-                 edge_in_dim, edge_h_dim,
-                 seq_in=False, num_layers=3, drop_rate=0.1, plm=True):
+    """
+
+    def __init__(
+        self,
+        node_in_dim,
+        node_h_dim,
+        edge_in_dim,
+        edge_h_dim,
+        seq_in=False,
+        num_layers=3,
+        drop_rate=0.1,
+        plm=True,
+    ):
 
         super(GVP_embedding, self).__init__()
-        
+
         # nn.Embedding(20,20)
         if seq_in and plm:
             self.W_s = nn.Embedding(20, 1280)
@@ -107,30 +124,29 @@ class GVP_embedding(nn.Module):
 
         self.W_v = nn.Sequential(
             LayerNorm(node_in_dim),
-            GVP(node_in_dim, node_h_dim, activations=(None, None))
+            GVP(node_in_dim, node_h_dim, activations=(None, None)),
         )
         self.W_e = nn.Sequential(
             LayerNorm(edge_in_dim),
-            GVP(edge_in_dim, edge_h_dim, activations=(None, None))
+            GVP(edge_in_dim, edge_h_dim, activations=(None, None)),
         )
 
         self.layers = nn.ModuleList(
-                GVPConvLayer(node_h_dim, edge_h_dim, drop_rate=drop_rate) 
-            for _ in range(num_layers))
-        
-        ns, _ = node_h_dim
-        self.W_out = nn.Sequential(
-            LayerNorm(node_h_dim),
-            GVP(node_h_dim, (ns, 0)))
+            GVPConvLayer(node_h_dim, edge_h_dim, drop_rate=drop_rate)
+            for _ in range(num_layers)
+        )
 
-    def forward(self, h_V, edge_index, h_E, seq):      
-        '''
+        ns, _ = node_h_dim
+        self.W_out = nn.Sequential(LayerNorm(node_h_dim), GVP(node_h_dim, (ns, 0)))
+
+    def forward(self, h_V, edge_index, h_E, seq):
+        """
         :param h_V: tuple (s, V) of node embeddings
         :param edge_index: `torch.Tensor` of shape [2, num_edges]
         :param h_E: tuple (s, V) of edge embeddings
         :param seq: if not `None`, int `torch.Tensor` of shape [num_nodes]
                     to be embedded and appended to `h_V`
-        '''
+        """
         if self.plm:
             seq = seq
             # print('plm')
@@ -174,6 +190,7 @@ class GVP_embedding(nn.Module):
 #         attention_weights = F.softmax(scores,dim=-1)  # [B, N_q, N_k]
 #         # attention_weights = self.dropout(attention_weights)
 
+
 #         # Weighted sum of values
 #         output = torch.matmul(attention_weights, v)
 #         return output
@@ -200,19 +217,30 @@ class AttentionBlock(nn.Module):
         B = query.shape[0]
 
         # Transform Q, K, V
-        q = self.f_q(query).view(B, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3) # [B, num_heads, N_q, head_dim]
-        k = self.f_k(key).view(B, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)  # [B, num_heads, N_k, head_dim]
+        q = (
+            self.f_q(query)
+            .view(B, -1, self.num_heads, self.head_dim)
+            .permute(0, 2, 1, 3)
+        )  # [B, num_heads, N_q, head_dim]
+        k = (
+            self.f_k(key).view(B, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        )  # [B, num_heads, N_k, head_dim]
 
-        v = self.f_v(value).view(B, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3) # [B, num_heads, N_k, head_dim]
-
+        v = (
+            self.f_v(value)
+            .view(B, -1, self.num_heads, self.head_dim)
+            .permute(0, 2, 1, 3)
+        )  # [B, num_heads, N_k, head_dim]
 
         # Calculate attention scores
-        scores = torch.matmul(q, k.transpose(-2, -1)) / self.sqrt_dk  # [B, num_heads, N_q, N_k]
+        scores = (
+            torch.matmul(q, k.transpose(-2, -1)) / self.sqrt_dk
+        )  # [B, num_heads, N_q, N_k]
         if mask is not None:
             mask = mask.unsqueeze(1)
             # print(mask.shape)
-            scores = scores.masked_fill(mask == 0, float('-inf'))
-       
+            scores = scores.masked_fill(mask == 0, float("-inf"))
+
         # Apply softmax and dropout
         if self.atten_active_fuc == "softmax":
             attention_weights = F.softmax(scores, dim=-1)
@@ -223,10 +251,12 @@ class AttentionBlock(nn.Module):
         # Apply attention to the values
         weighted = torch.matmul(attention_weights, v)  # [B, num_heads, N_q, head_dim]
         # print(weighted.shape)
-        weighted = weighted.permute(0, 2, 1, 3).contiguous() # [B, N_q, num_heads, head_dim]
+        weighted = weighted.permute(
+            0, 2, 1, 3
+        ).contiguous()  # [B, N_q, num_heads, head_dim]
 
         # Concatenate heads and put through final linear layer
-        weighted = weighted.view(B, -1, self.hid_dim)   # [B, N_q, hid_dim]
+        weighted = weighted.view(B, -1, self.hid_dim)  # [B, N_q, hid_dim]
         output = self.fc_out(weighted)
 
         return output
@@ -235,8 +265,13 @@ class AttentionBlock(nn.Module):
 class CrossAttentionBlock(nn.Module):
     def __init__(self, hid_dim, dropout, atten_active_fuc, num_heads=4):
         super(CrossAttentionBlock, self).__init__()
-        self.att = AttentionBlock(hid_dim=hid_dim, num_heads=num_heads, atten_active_fuc=atten_active_fuc, dropout=dropout)
-        
+        self.att = AttentionBlock(
+            hid_dim=hid_dim,
+            num_heads=num_heads,
+            atten_active_fuc=atten_active_fuc,
+            dropout=dropout,
+        )
+
         self.linear_res = nn.Sequential(
             nn.Linear(hid_dim, hid_dim),
             nn.Dropout(dropout),
@@ -263,24 +298,42 @@ class CrossAttentionBlock(nn.Module):
         mask_aa_expanded = mask_aa.unsqueeze(1)  # [B, 1, N_aa]
 
         # 交叉注意力计算
-        aa_att = self.att(aa_features, ligand_features, ligand_features, mask=mask_l_expanded)
-        lig_att = self.att(ligand_features, aa_features, aa_features, mask=mask_aa_expanded)
+        aa_att = self.att(
+            aa_features, ligand_features, ligand_features, mask=mask_l_expanded
+        )
+        lig_att = self.att(
+            ligand_features, aa_features, aa_features, mask=mask_aa_expanded
+        )
         # 线性变换与残差连接
         aa_features = self.linear_res(aa_att) + aa_features
-        ligand_features = self.linear_lig(lig_att)+ ligand_features #
+        ligand_features = self.linear_lig(lig_att) + ligand_features  #
 
         return ligand_features, aa_features
 
+
 class InteractionBlock(nn.Module):
-    def __init__(self, hid_dim, num_heads, dropout, atten_active_fuc, crossAttention=True, interact_type="product"):
+    def __init__(
+        self,
+        hid_dim,
+        num_heads,
+        dropout,
+        atten_active_fuc,
+        crossAttention=True,
+        interact_type="product",
+    ):
         super(InteractionBlock, self).__init__()
         self.dropout = dropout
         self.hid_dim = hid_dim
         self.interact_type = interact_type
         self.crossAttention = crossAttention
         if self.crossAttention:
-            self.prot_lig_attention = CrossAttentionBlock(hid_dim=hid_dim, dropout=dropout, atten_active_fuc = atten_active_fuc, num_heads=num_heads)
-        
+            self.prot_lig_attention = CrossAttentionBlock(
+                hid_dim=hid_dim,
+                dropout=dropout,
+                atten_active_fuc=atten_active_fuc,
+                num_heads=num_heads,
+            )
+
         if self.interact_type == "product":
 
             self.mlp = nn.Sequential(
@@ -291,12 +344,11 @@ class InteractionBlock(nn.Module):
             )
         else:
             self.mlp = nn.Sequential(
-                nn.Linear(hid_dim*2, hid_dim),
+                nn.Linear(hid_dim * 2, hid_dim),
                 nn.BatchNorm1d(hid_dim),
                 nn.Dropout(dropout),
                 nn.LeakyReLU(),
             )
-        
 
         self.lig_trans_inteact = nn.Sequential(
             nn.Linear(hid_dim, hid_dim),
@@ -316,43 +368,48 @@ class InteractionBlock(nn.Module):
         #     nn.Linear(hid_dim, hid_dim),
         #     nn.LeakyReLU(),
         #     nn.Linear(hid_dim, 4*2), # 4 side chain angles for each amino acid(sine and cosine)
-        #     nn.LeakyReLU(),            
+        #     nn.LeakyReLU(),
         # )
 
     def forward(self, lig_features, aa_features, lig_mask=None, aa_mask=None):
-            # Apply transformations to ligand and amino acid features
-            if self.crossAttention:
-                lig_features, aa_features = self.prot_lig_attention(lig_features, aa_features, lig_mask, aa_mask)
-            
-            lig_features = lig_features.unsqueeze(2)  # [batch_size, N_l, 1, hidden]
-            # print('lig_features_exp',lig_features_exp.shape)
-            aa_features = aa_features.unsqueeze(1)  # [batch_size, 1, N_aa, hidden]
-            # print('aa_features_exp',aa_features_exp.shape)
-            if self.interact_type == "product":
-                lig_features = self.lig_trans_inteact(lig_features)
-                aa_features = self.aa_trans_inteact(aa_features)
-                interaction = torch.multiply(lig_features, aa_features) 
+        # Apply transformations to ligand and amino acid features
+        if self.crossAttention:
+            lig_features, aa_features = self.prot_lig_attention(
+                lig_features, aa_features, lig_mask, aa_mask
+            )
 
-            else:
-                N_l =lig_features.size(1)
-                N_p =aa_features.size(2)
+        lig_features = lig_features.unsqueeze(2)  # [batch_size, N_l, 1, hidden]
+        # print('lig_features_exp',lig_features_exp.shape)
+        aa_features = aa_features.unsqueeze(1)  # [batch_size, 1, N_aa, hidden]
+        # print('aa_features_exp',aa_features_exp.shape)
+        if self.interact_type == "product":
+            lig_features = self.lig_trans_inteact(lig_features)
+            aa_features = self.aa_trans_inteact(aa_features)
+            interaction = torch.multiply(lig_features, aa_features)
 
-                lig_features = self.lig_trans_inteact(lig_features)
-                lig_features = lig_features.repeat(1,1,N_p,1)
-                # print('lig_features',lig_features.shape)
-                aa_features = self.aa_trans_inteact(aa_features)
-                aa_features = aa_features.repeat(1,N_l,1,1)
+        else:
+            N_l = lig_features.size(1)
+            N_p = aa_features.size(2)
 
-                interaction = torch.cat((lig_features, aa_features), dim=-1) # [batch_size, N_l, N_aa, 2*hidden]
+            lig_features = self.lig_trans_inteact(lig_features)
+            lig_features = lig_features.repeat(1, 1, N_p, 1)
+            # print('lig_features',lig_features.shape)
+            aa_features = self.aa_trans_inteact(aa_features)
+            aa_features = aa_features.repeat(1, N_l, 1, 1)
 
-            if lig_mask is not None and aa_mask is not None:
-                lig_mask_exp = lig_mask.unsqueeze(2)  # [batch_size, N_l, 1]
-                aa_mask_exp = aa_mask.unsqueeze(1)  # [batch_size, 1, N_aa]
-                interaction_mask = lig_mask_exp & aa_mask_exp  # [batch_size, N_l, N_aa]
-            interaction = interaction[interaction_mask]
-            interaction = self.mlp(interaction)
-            return interaction, interaction_mask#, angles
-    
+            interaction = torch.cat(
+                (lig_features, aa_features), dim=-1
+            )  # [batch_size, N_l, N_aa, 2*hidden]
+
+        if lig_mask is not None and aa_mask is not None:
+            lig_mask_exp = lig_mask.unsqueeze(2)  # [batch_size, N_l, 1]
+            aa_mask_exp = aa_mask.unsqueeze(1)  # [batch_size, 1, N_aa]
+            interaction_mask = lig_mask_exp & aa_mask_exp  # [batch_size, N_l, N_aa]
+        interaction = interaction[interaction_mask]
+        interaction = self.mlp(interaction)
+        return interaction, interaction_mask  # , angles
+
+
 class Intrablock(nn.Module):
     def __init__(self, hid_dim, dropout):
         super(Intrablock, self).__init__()
@@ -391,14 +448,15 @@ class Intrablock(nn.Module):
             mask_l_1 = mask_l.unsqueeze(1)
             mask_l_2 = mask_l.unsqueeze(2)
             mask_l = mask_l_1 & mask_l_2
-            mask_l = mask_l & ~(torch.eye(mask_l.shape[1], dtype=torch.bool, device=mask_l.device))
+            mask_l = mask_l & ~(
+                torch.eye(mask_l.shape[1], dtype=torch.bool, device=mask_l.device)
+            )
             interaction = interaction[mask_l]
 
         # Apply MLP to interaction features
         interaction = self.mlp(interaction)
 
         return interaction, mask_l
-
 
 
 # %%
